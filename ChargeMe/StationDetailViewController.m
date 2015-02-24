e37bcc5febeda4823a61093655059c4381104004
smkyaw <seaverkyaw@gmail.com>
2015-02-24 10:12:30 -0800

    phone call

--- ChargeMe/StationDetailViewController.m
+++ ChargeMe/StationDetailViewController.m
@@ -232,6 +232,9 @@
 
 }
 
+-(IBAction)callPhone:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.chargingStation.stationPhone]];
+}
 #pragma mark Tableview Methods
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
